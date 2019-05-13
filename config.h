485f7b4ec50ef155f3c67d03476cfdd0a750a44c
Jan Christoph Bischko <jbischko@barracuda.com>
2019-05-13 18:08:41 +0200

    Faster mouse scroll

--- config.h
+++ config.h
@@ -208,8 +208,8 @@ MouseKey mkeys[] = {
 	/* button               mask            function        argument */
 	{ Button4,              ShiftMask,      kscrollup,      {.i =  1} },
 	{ Button5,              ShiftMask,      kscrolldown,    {.i =  1} },
-	{ Button4,              XK_NO_MOD,      kscrollup,      {.i =  1} },
-	{ Button5,              XK_NO_MOD,      kscrolldown,    {.i =  1} },
+	{ Button4,              XK_NO_MOD,      kscrollup,      {.i =  3} },
+	{ Button5,              XK_NO_MOD,      kscrolldown,    {.i =  3} },
 	{ Button4,              TERMMOD,        zoom,           {.f =  +1} },
 	{ Button5,              TERMMOD,        zoom,           {.f =  -1} },
 };
